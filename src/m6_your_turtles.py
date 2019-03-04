b5b870362173a909d5f8e5fed85ad4401bf91978
wilfonba <wilfonba@rose-hulman.edu>
2019-03-04 10:33:46 -0500

    Message

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -28,3 +28,27 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+t1 = rg.SimpleTurtle()
+t1.pen = rg.Pen('black', 3)
+t1.speed = 20
+r = 100
+c = -200
+
+for k in range(3):
+    t1.pen_up()
+    t1.go_to(rg.Point(0,c))
+    t1.pen_down()
+    t1.draw_circle(r)
+    r = r - 20
+    c = c + 2*r
+
+t2 = rg.SimpleTurtle()
+t2.pen = rg.Pen('black', 2)
+t2.speed = 20
+
+window.close_on_mouse_click()
